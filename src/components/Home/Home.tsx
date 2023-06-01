d16fa692a7bad993408909e6159d413ba0950401
Jonathan Kelman <kelman.jonathan@gmail.com>
2023-06-01 16:37:42 +0300

    home feed

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Home.css";
 import NavBar from "../NavBar/NavBar";
 import SideMenu from "../SideMenu/SideMenu";
 
 const Home = () => {
+	const [tweet, setTweet] = useState<string>("");
+	console.log(tweet);
+
+	function handleTweetInput(e: any) {
+		setTweet(e.target.value);
+	}
+
 	return (
 		<div id="home_master">
 			<NavBar />
@@ -15,11 +22,13 @@ const Home = () => {
 							<div>Profile Pic</div>
 							<div className="tweet_form-container">
 								<form>
-									<textarea name="" id="tweet_form" placeholder="What's Happening?!"></textarea>
+									<textarea name="" id="tweet_form" placeholder="What's Happening?!" onChange={handleTweetInput}></textarea>
 									<div className="tweet_form-options-container">
 										<div>Media upload options</div>
 										<div className="tweet_button">
-											<div>145</div>
+											<div className={"tweet_charachters-main"}>
+												<span>{145 - tweet.length}</span>
+											</div>
 											<div>
 												<button>Tweet</button>
 											</div>

adba1db8f0a4572246329b14fe489a143afd1c76
Jonathan Kelman <kelman.jonathan@gmail.com>
2023-06-11 21:13:32 +0300

    added profile section

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -8,6 +8,8 @@ import EditProfile from "./EditProfile";
 import { APIHandler } from "../../fireBaseConfig";
 import { profileContext } from "../../context/ProfileContext";
 import { ITweet } from "../../global/interfaces";
+import NavBar from "../NavBar/NavBar";
+import SideMenu from "../SideMenu/SideMenu";
 
 const Profile = () => {
 	const [screenView, setScreenView] = useState(1);
@@ -34,81 +36,87 @@ const Profile = () => {
 	if (loading || !currentUser || !userProfileInfo) return <>Loading</>;
 	return (
 		<div id="profile_master">
-			<div className={editProfile ? "prompt_blur_section" : undefined}>
-				<section id="profile_header">
-					<div className="profile_user_name">{userProfileInfo.name}</div>
-					<div className="user_tweets_length">{`${userTweets?.length} tweets`}</div>
-				</section>
-				<section id="profile_pics">
-					<div className="wallPaper_Container">
-						<img src={userProfileInfo.wallpaperPic || defaultWallpaper} alt="" />
-					</div>
-					{currentUser.uid === userProfileInfo.uid && (
-						<div className="edit_container">
-							<img src={userProfileInfo.profilePic} alt="" className="profile_pic_container" />
-							<button
+			<span className={editProfile ? "prompt_blur_section" : undefined}>
+				<NavBar />
+				<div className="main_profile_container">
+					<div>
+						<section id="profile_header">
+							<div className="profile_user_name">{userProfileInfo.name}</div>
+							<div className="user_tweets_length">{`${userTweets?.length} tweets`}</div>
+						</section>
+						<section id="profile_pics">
+							<div className="wallPaper_Container">
+								<img src={userProfileInfo.wallpaperPic || defaultWallpaper} alt="" />
+							</div>
+							{currentUser.uid === userProfileInfo.uid && (
+								<div className="edit_container">
+									<img src={userProfileInfo.profilePic} alt="" className="profile_pic_container" />
+									<button
+										onClick={() => {
+											setEditProfile(true);
+										}}
+									>
+										Edit profile
+									</button>
+								</div>
+							)}
+						</section>
+
+						<section id="profile_name">
+							<div>{userProfileInfo.name}</div>
+							<div>{`@${userProfileInfo.name}`}</div>
+							<div>
+								<MdCalendarMonth />
+								<div>{new Date(Number(userProfileInfo.joinedDate)).toLocaleDateString()}</div>
+							</div>
+							<div>
+								<div>1 following</div>
+								<div>0 followers</div>
+							</div>
+						</section>
+
+						<section id="profile_filters">
+							<div
+								className={screenView === 1 ? "active" : undefined}
 								onClick={() => {
-									setEditProfile(true);
+									setScreenView(1);
 								}}
 							>
-								Edit profile
-							</button>
-						</div>
-					)}
-				</section>
-
-				<section id="profile_name">
-					<div>{userProfileInfo.name}</div>
-					<div>{`@${userProfileInfo.name}`}</div>
-					<div>
-						<MdCalendarMonth />
-						<div>{new Date(Number(userProfileInfo.joinedDate)).toLocaleDateString()}</div>
-					</div>
-					<div>
-						<div>1 following</div>
-						<div>0 followers</div>
-					</div>
-				</section>
-
-				<section id="profile_filters">
-					<div
-						className={screenView === 1 ? "active" : undefined}
-						onClick={() => {
-							setScreenView(1);
-						}}
-					>
-						Tweets
-					</div>
-					<div
-						className={screenView === 2 ? "active" : undefined}
-						onClick={() => {
-							setScreenView(2);
-							// getUserLikes();
-						}}
-					>
-						Likes
-					</div>
-					<div
-						className={screenView === 3 ? "active" : undefined}
-						onClick={() => {
-							setScreenView(3);
-						}}
-					>
-						Following
-					</div>
-				</section>
-				<section id="profile_suggestions">
-					{screenView === 1 &&
-						userTweets?.map((tweet) => {
-							return <TweetCard tweet={tweet} loading={false} currentUser={currentUser} />;
-						})}
-					{/* {screenView === 2 &&
+								Tweets
+							</div>
+							<div
+								className={screenView === 2 ? "active" : undefined}
+								onClick={() => {
+									setScreenView(2);
+									// getUserLikes();
+								}}
+							>
+								Likes
+							</div>
+							<div
+								className={screenView === 3 ? "active" : undefined}
+								onClick={() => {
+									setScreenView(3);
+								}}
+							>
+								Following
+							</div>
+						</section>
+						<section id="profile_suggestions">
+							{screenView === 1 &&
+								userTweets?.map((tweet) => {
+									return <TweetCard tweet={tweet} loading={false} currentUser={currentUser} />;
+								})}
+							{/* {screenView === 2 &&
 						likedTweets?.map((tweet: ITweet) => {
 							return <TweetCard tweet={tweet} loading={false} currentUser={currentUser} />;
 						})} */}
-				</section>
-			</div>
-			{/* <EditProfile editProfile={editProfile} setEditProfile={setEditProfile} /> */}
+						</section>
+					</div>
+					<SideMenu />
+				</div>
+				{/* <EditProfile editProfile={editProfile} setEditProfile={setEditProfile} /> */}
+			</span>
 		</div>
 	);
 };

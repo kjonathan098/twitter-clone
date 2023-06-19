3f04269d4c2745eafb158d42f767b08e9d6f9b61
Jonathan Kelman <kelman.jonathan@gmail.com>
2023-06-19 14:56:58 +0300

    added imag upload and preview

--- src/components/Home/CreateTweet.tsx
+++ src/components/Home/CreateTweet.tsx
@@ -1,11 +1,16 @@
-import { useContext, useState } from "react";
+import { useContext, useState, useRef } from "react";
 import "./CreateTweet.css";
 import { authContext } from "../../context/AuthContext";
 import ProfilePic from "../Utils/ProfilePic";
+import { AiOutlinePicture, AiFillCloseCircle } from "react-icons/ai";
 
 const CreateTweet = () => {
 	const [tweet, setTweet] = useState<string>("");
+	const [uploadMedia, setUploadMedia] = useState<File | null>(null);
+	const [mediaPreview, setMediaPreview] = useState<string | null>(null);
+
 	const { currentUser } = useContext(authContext);
+	const tweetMediaRef: React.RefObject<HTMLInputElement> = useRef<HTMLInputElement>(null);
 
 	function handleTweetInput(e: React.ChangeEvent<HTMLTextAreaElement>) {
 		setTweet(e.target.value);
@@ -24,14 +29,43 @@ const CreateTweet = () => {
 		);
 	}
 
+	const handleMediaUpload = async (e: React.ChangeEvent<HTMLInputElement>) => {
+		setUploadMedia(e.target.files![0]);
+		const [imageFile]: any = e.target.files;
+		const localURL = URL.createObjectURL(imageFile);
+		setMediaPreview(localURL);
+	};
+
 	return (
 		<div className="create_tweet-main">
 			<div>{currentUser && <ProfilePic profilePic={currentUser.profilePic} size={80} openUserProfile={false} />}</div>
 			<div className="tweet_form-container">
-				<form onSubmit={uploadNewTweet}>
+				<form onSubmit={uploadNewTweet} >
 					<textarea name="" id="tweet_form" placeholder="What's Happening?!" onChange={handleTweetInput}></textarea>
+					{mediaPreview && (
+						<div className="img_container">
+							<AiFillCloseCircle
+								className="delete_media"
+								onClick={() => {
+									setMediaPreview(null);
+								}}
+							/>
+							<img src={mediaPreview} alt="" />
+						</div>
+					)}
+
 					<div className="tweet_form-options-container">
-						<div>Media upload options</div>
+						<div>
+							<div
+								className="upload_media_container"
+								onClick={() => {
+									tweetMediaRef?.current?.click();
+								}}
+							>
+								<AiOutlinePicture className="upload_media_icon" />
+								<input type="file" style={{ display: "none" }} ref={tweetMediaRef} onChange={handleMediaUpload} />
+							</div>
+						</div>
 						<div className="tweet_button">
 							{handleTweetCounter()}
 							<div>

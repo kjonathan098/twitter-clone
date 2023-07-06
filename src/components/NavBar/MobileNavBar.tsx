aeb311816418a6fdb0162ec96ae407cd73062456
Jonathan Kelman <kelman.jonathan@gmail.com>
2023-07-06 17:10:28 +0300

    ui bug fixes

--- src/components/NavBar/MobileNavBar.tsx
+++ src/components/NavBar/MobileNavBar.tsx
@@ -1,23 +1,22 @@
 import { useContext } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { displayNavContext } from "../../context/DisplayNavContext";
+import "./MobileNavBar.css";
 
 const MobileNavBar = () => {
 	const { displayNav, setDisplayNav } = useContext(displayNavContext);
 
 	return (
-		<>
-			<div className="menu_button_container">
-				{!displayNav && (
-					<GiHamburgerMenu
-						className="menu_button"
-						onClick={() => {
-							setDisplayNav(!displayNav);
-						}}
-					/>
-				)}
-			</div>
-		</>
+		<div className="menu_button_container">
+			{!displayNav && (
+				<GiHamburgerMenu
+					className="menu_button"
+					onClick={() => {
+						setDisplayNav(!displayNav);
+					}}
+				/>
+			)}
+		</div>
 	);
 };
 

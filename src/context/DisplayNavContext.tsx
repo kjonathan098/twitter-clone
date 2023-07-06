6f2b4497dee6a707e556a5e4e5c74356df37705c
Jonathan Kelman <kelman.jonathan@gmail.com>
2023-07-06 17:03:47 +0300

    display nav context

---
+++ src/context/DisplayNavContext.tsx
@@ -0,0 +1,19 @@
+import React, { createContext, useEffect, useState } from "react";
+
+interface DisplayNavContextValue {
+	test: string;
+	displayNav: boolean;
+	setDisplayNav: React.Dispatch<React.SetStateAction<boolean>>;
+}
+export const displayNavContext = createContext<DisplayNavContextValue>({} as DisplayNavContextValue);
+
+interface IProps {
+	children: React.ReactNode;
+}
+const DisplayNavProvider: React.FC<IProps> = ({ children }) => {
+	const [displayNav, setDisplayNav] = useState(false);
+	const test = "test";
+	return <displayNavContext.Provider value={{ test, displayNav, setDisplayNav }}>{children}</displayNavContext.Provider>;
+};
+
+export default DisplayNavProvider;
